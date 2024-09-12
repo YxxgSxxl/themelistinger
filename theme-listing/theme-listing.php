1c518b19584d561e97d291be3eef89a51cd312e9
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-09-12 10:58:54 +0200

    feat: functions added

--- theme-listing/theme-listing.php
+++ theme-listing/theme-listing.php
@@ -4,9 +4,19 @@
 Plugin Name: Own Plugin (themes listinger)
 Description: A simple security plugin to list all themes
 Version: 1.0
-Author: IUT Mulhouse
+Author: YxxgSxxl
 */
 
 if(!defined("ABSPATH")) {
-    exit;
+    exit; // Interdit l'accès direct
+}
+
+function theme_listing_page(): void {
+    echo "<div>
+            <h1>Ma page dans l'Admin panel</h1>
+    </div>";
+}
+
+function theme_listing_menu(): void {
+    add_menu_page('Titre page', 'Titre menu','','','');
 }

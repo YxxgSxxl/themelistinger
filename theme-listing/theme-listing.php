62e6296ec4c565605d3432252292243a6c419434
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-09-12 10:51:09 +0200

    :tada: Project uploaded :tada:

---
+++ theme-listing/theme-listing.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Own Plugin (themes listinger)
+Description: A simple security plugin to list all themes
+Version: 1.0
+Author: IUT Mulhouse
+*/
+
+if(!defined("ABSPATH")) {
+    exit;
+}

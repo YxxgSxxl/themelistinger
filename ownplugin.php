c0984c2972d1dacfd79ff0f276ca65362ae8cb73
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-09-12 11:17:35 +0200

    fix: plugin is now working (listed in plugins)

--- ownplugin.php
+++ ownplugin.php
@@ -19,11 +19,21 @@ function theme_listing_page(): void {
 }
 
 function theme_listing_menu(): void {
+    // Ajoute le menu dans l'Admin panel de WordPress
     add_menu_page('Titre page', 'Theme Listinger','manage_options','idmenu', 'theme_listing_page', 'dashicons-code-standards', 5);
 }
 
 add_action('admin_menu','theme_listing_menu');
 
 function themedir_list(): void {
-    $themedir = scandir(get_theme_root() .'');
+    // Liste tout les thèmes retrouvés dans le fichier racine des thèmes du moteur WordPress
+    $themesdir = scandir(get_theme_root() .'');
+
+    echo "<h2>Liste de tous les themes</h2>";
+
+    foreach($themesdir as $onedir) {
+        echo '<ul>';
+        echo '<li>' . $onedir . '</li>';
+        echo '</ul>';
+    }
 }
